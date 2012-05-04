06683507decb623bcca56e05996e37a01d7f1354
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2012-05-04 22:15:39 +0100

    initial commit

---
+++ JonSkeetDesignPatterns/License.cs
@@ -0,0 +1,21 @@
+﻿using NodaTime;
+
+namespace JonSkeetDesignPatterns
+{
+    public class License
+    {
+        private readonly Instant _expiry;
+        private readonly IClock _clock;
+
+        public License(Instant expiry, IClock clock)
+        {
+            _expiry = expiry;
+            _clock = clock;
+        }
+
+        public bool HasExpired
+        {
+            get { return _clock.Now >= _expiry; }
+        }
+    }
+}
